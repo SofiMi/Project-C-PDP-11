6cbd87a3f1bb689426c57e6117d95388ed3f592f
SofiMi <maslennikova.si@phystech.edu>
2022-04-20 21:32:16 +0300

    Реализована функция w_write

--- PDP-11/main.c
+++ PDP-11/main.c
@@ -1,8 +1,14 @@
-#include "define.c"
-//#include "test.c"
+#include <stdlib.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef unsigned char byte; //8 bit
+typedef unsigned short int word; //16 bit
+typedef word Adress;        // 16 bit
+
+#define MEMSIZE (64*1024)
 
 byte mem[MEMSIZE];
-
 void b_write(Adress adr, byte b);
 byte b_read(Adress adr);
 void w_write(Adress adr, word w);
@@ -15,16 +21,24 @@ void test_mem()
     b_write(2, b0);
     byte bres = b_read(2);
     printf ("%02hhx == %02hhx\n", b0, bres);
+    assert(b0 == bres);
 
     // Пишем 2 байта - читаем одно слово
     Adress a = 4;
-    byte b1 = 0xcb;
+    byte b1 = 0x0b;
     b0 = 0x0a;
     word w = 0x0b0a;
     b_write(a, b0);
     b_write(a + 1, b1);
     word wres = w_read(a);
     printf("word_write/byte_read \t %04hx == %02hhx%02hhx\n", wres, b1, b0);
+    assert(w == wres);
+
+    Adress a2 = 6;
+    word w1 = 0x0b0a;
+    w_write(a2, w1);
+    word wres2 = w_read(a2);
+    printf("word_write/byte_read \t %04hx == %04hx\n", wres2, w1);
 }
 
 int main(){
@@ -42,7 +56,14 @@ byte b_read(Adress adr)
     return mem[adr];
 }
 
-void w_write(Adress adr, word w);
+void w_write(Adress adr, word w)
+{
+   // Записываем в первый байт
+   b_write(adr + 1, w >> 8);
+   // Записываем во второй байт
+   b_write(adr, w & 0xFF);
+}
+
 word w_read(Adress adr)
 {
     // Читаем первый байт
