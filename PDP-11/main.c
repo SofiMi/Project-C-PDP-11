38723b91692ee36f1859008c128bf80ca5808d22
SofiMi <maslennikova.si@phystech.edu>
2022-04-21 01:11:17 +0300

    Исправлены ошибки, связанные с понимание байтово-словной системы

--- PDP-11/main.c
+++ PDP-11/main.c
@@ -9,6 +9,7 @@ typedef word Adress;        // 16 bit
 #define MEMSIZE (64*1024)
 
 byte mem[MEMSIZE];
+
 void b_write(Adress adr, byte b);
 byte b_read(Adress adr);
 void w_write(Adress adr, word w);
@@ -41,7 +42,7 @@ void test_mem()
     word w_test_3 = 0x0b0c;
     w_write(adr_test_3, w_test_3);
     byte b0res_test_3 = b_read(adr_test_3);
-    byte b1res_test_3 = b_read(adr_test_3 + 2);
+    byte b1res_test_3 = b_read(adr_test_3 + 1);
     word wres_test_3 = (b1res_test_3 << 8)|b0res_test_3;
     printf("word_write/byte_read \t %04hx == %04hx\n", w_test_3, wres_test_3);
     assert(w_test_3 == wres_test_3);
@@ -53,7 +54,7 @@ void test_mem()
     byte b0_test_4 = 0x0a;
     word w_test_4 = 0x0b0a;
     b_write(adr_test_4, b0_test_4);
-    b_write(adr_test_4 + 2, b1_test_4);
+    b_write(adr_test_4 + 1, b1_test_4);
     word wres_test_4 = w_read(adr_test_4);
     printf("byte_write/word_read \t %04hx == %02hhx%02hhx\n", wres_test_4, b1_test_4, b0_test_4);
     assert(w_test_4 == wres_test_4);
@@ -65,8 +66,6 @@ void test_mem()
     word w_test_5 = 0x0b0a;
     // We comment so as not to be scared every time:
     // if we write an odd address, the program will immediately tell us about it
-    //b_write(adr_test_5, b_test_5);
-    //b_read(adr_test_5);
     //w_write(adr_test_5, w_test_5);
     //w_read(adr_test_5);
 
@@ -80,20 +79,19 @@ int main(){
 
 void b_write(Adress adr, byte b)
 {
-    assert(0 == adr % 2);
     mem[adr] = b;
 }
 
 byte b_read(Adress adr)
 {
-    assert(0 == adr % 2);
     return mem[adr];
 }
 
 void w_write(Adress adr, word w)
 {
+   assert(0 == adr % 2);
    // Write to the 1 byte
-   b_write(adr + 2, w >> 8);
+   b_write(adr + 1, w >> 8);
    // Write to the 2 byte
    b_write(adr, w & 0xFF);
 }
@@ -102,7 +100,7 @@ word w_read(Adress adr)
 {
     assert(0 == adr % 2);
     // Read to the 1 byte
-    word w = ((word)mem[adr + 2]) << 8;
+    word w = ((word)mem[adr + 1]) << 8;
     // Read to the 2 byte
     w = w | mem[adr];
     return w;
@@ -114,14 +112,14 @@ void load_file()
 
     Adress adr_start_block;
     int number_byte;
-    word now_word;
-    while (scanf ("%x %x", &adr_start_block, &number_byte) == 2)
+    byte now_byte;
+    while (fscanf ("%x %x", &adr_start_block, &number_byte) == 2)
     {
         for (int i = 0; i < number_byte; i++)
             {
-                scanf ("%x", &now_word);
-                b_write (adr_start_block, now_word);
-                adr_start_block += 2;
+                scanf ("%x", &now_byte);
+                b_write (adr_start_block, now_byte);
+                adr_start_block++;
             }
     }
     fclose(fin);
