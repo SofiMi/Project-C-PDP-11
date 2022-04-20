a2c3411c90a82f632734921215734ab91d8b843c
SofiMi <maslennikova.si@phystech.edu>
2022-04-21 00:23:30 +0300

    Добавлена функция load_file (без тестов), которая может считывать данные с файла data.txt(определенное имя - будет изменено)

--- PDP-11/main.c
+++ PDP-11/main.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
@@ -13,11 +13,12 @@ void b_write(Adress adr, byte b);
 byte b_read(Adress adr);
 void w_write(Adress adr, word w);
 word w_read(Adress adr);
+void load_file();
 
 void test_mem()
 {
-    // Тест 1:
-    // Пишем байт, читаем байт
+    // Test 1:
+    // Write byte, read byte
     Adress adr_test_1 = 2;
     byte b_test_1 = 0x0a;
     b_write(adr_test_1, b_test_1);
@@ -25,8 +26,8 @@ void test_mem()
     printf ("byte_write/byte_read \t %02hhx == %02hhx\n", b_test_1, bres_test_1);
     assert (b_test_1 == bres_test_1);
 
-    // Тест 2:
-    // Пишем слово - читаем слово
+    // Test 2:
+    // Write word, read word
     Adress adr_test_2 = 4;
     word w_test_2 = 0x0bac;
     w_write(adr_test_2, w_test_2);
@@ -34,8 +35,8 @@ void test_mem()
     printf("word_write/word_read \t %04hx == %04hx\n", wres_test_2, w_test_2);
     assert(w_test_2 == wres_test_2);
 
-    // Тест 3:
-    // Пишем слово - читаем 2 байта
+    // Test 3:
+    // Write word, read 2 byte
     Adress adr_test_3 = 6;
     word w_test_3 = 0x0b0c;
     w_write(adr_test_3, w_test_3);
@@ -45,8 +46,8 @@ void test_mem()
     printf("word_write/byte_read \t %04hx == %04hx\n", w_test_3, wres_test_3);
     assert(w_test_3 == wres_test_3);
 
-    // Тест 4:
-    // Пишем 2 байта - читаем одно слово
+    // Test 4:
+    // Write 2 byte - write word
     Adress adr_test_4 = 8;
     byte b1_test_4 = 0x0b;
     byte b0_test_4 = 0x0a;
@@ -57,13 +58,13 @@ void test_mem()
     printf("byte_write/word_read \t %04hx == %02hhx%02hhx\n", wres_test_4, b1_test_4, b0_test_4);
     assert(w_test_4 == wres_test_4);
 
-    // Тест 5:
-    // Проверяем неработоспособность программы при написании нечетного адреса
+    // Test 5:
+    // We check if the program is inoperable when writing an odd address
     Adress adr_test_5 = 7;
     byte b_test_5 = 0x0a;
     word w_test_5 = 0x0b0a;
-    // Комментируем, чтобы не пугаться каждый раз:
-    // если мы напишем нечетный адрес, то программа сразу нам об этом скажет
+    // We comment so as not to be scared every time:
+    // if we write an odd address, the program will immediately tell us about it
     //b_write(adr_test_5, b_test_5);
     //b_read(adr_test_5);
     //w_write(adr_test_5, w_test_5);
@@ -73,6 +74,7 @@ void test_mem()
 
 int main(){
     test_mem();
+    load_file();
     return 0;
 }
 
@@ -90,18 +92,37 @@ byte b_read(Adress adr)
 
 void w_write(Adress adr, word w)
 {
-   // Записываем в первый байт
+   // Write to the 1 byte
    b_write(adr + 2, w >> 8);
-   // Записываем во второй байт
+   // Write to the 2 byte
    b_write(adr, w & 0xFF);
 }
 
 word w_read(Adress adr)
 {
     assert(0 == adr % 2);
-    // Читаем первый байт
+    // Read to the 1 byte
     word w = ((word)mem[adr + 2]) << 8;
-    // Читаем второй байт
+    // Read to the 2 byte
     w = w | mem[adr];
     return w;
+}
+
+void load_file()
+{
+    FILE * fin  = fopen("data.txt", "r");
+
+    Adress adr_start_block;
+    int number_byte;
+    word now_word;
+    while (scanf ("%x %x", &adr_start_block, &number_byte) == 2)
+    {
+        for (int i = 0; i < number_byte; i++)
+            {
+                scanf ("%x", &now_word);
+                b_write (adr_start_block, now_word);
+                adr_start_block += 2;
+            }
+    }
+    fclose(fin);
 }
