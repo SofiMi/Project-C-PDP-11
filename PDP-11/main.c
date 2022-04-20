5aa3e7e78ce271a227865edaec5b1effaa1628f4
SofiMi <maslennikova.si@phystech.edu>
2022-04-20 21:50:18 +0300

    Добавлены тесты для положительных чисел

--- PDP-11/main.c
+++ PDP-11/main.c
@@ -16,29 +16,46 @@ word w_read(Adress adr);
 
 void test_mem()
 {
-    byte b0 = 0x0a;
+    // Тест 1:
     // Пишем байт, читаем байт
-    b_write(2, b0);
-    byte bres = b_read(2);
-    printf ("%02hhx == %02hhx\n", b0, bres);
-    assert(b0 == bres);
+    Adress adr_test_1 = 2;
+    byte b_test_1 = 0x0a;
+    b_write(adr_test_1, b_test_1);
+    byte bres_test_1 = b_read(adr_test_1);
+    printf ("byte_write/byte_read \t %02hhx == %02hhx\n", b_test_1, bres_test_1);
+    assert (b_test_1 == bres_test_1);
 
+    // Тест 2:
+    // Пишем слово - читаем слово
+    Adress adr_test_2 = 4;
+    word w_test_2 = 0x0bac;
+    w_write(adr_test_2, w_test_2);
+    word wres_test_2 = w_read(adr_test_2);
+    printf("word_write/word_read \t %04hx == %04hx\n", wres_test_2, w_test_2);
+    assert(w_test_2 == wres_test_2);
+
+    // Тест 3:
+    // Пишем слово - читаем 2 байта
+    Adress adr_test_3 = 6;
+    word w_test_3 = 0x0b0c;
+    w_write(adr_test_3, w_test_3);
+    byte b0res_test_3 = b_read(adr_test_3);
+    byte b1res_test_3 = b_read(adr_test_3 + 1);
+    word wres_test_3 = (b1res_test_3 << 8)|b0res_test_3;
+    printf("word_write/byte_read \t %04hx == %04hx\n", w_test_3, wres_test_3);
+    assert(w_test_3 == wres_test_3);
+
+    // Тест 4:
     // Пишем 2 байта - читаем одно слово
-    Adress a = 4;
-    byte b1 = 0x0b;
-    b0 = 0x0a;
-    word w = 0x0b0a;
-    b_write(a, b0);
-    b_write(a + 1, b1);
-    word wres = w_read(a);
-    printf("word_write/byte_read \t %04hx == %02hhx%02hhx\n", wres, b1, b0);
-    assert(w == wres);
-
-    Adress a2 = 6;
-    word w1 = 0x0b0a;
-    w_write(a2, w1);
-    word wres2 = w_read(a2);
-    printf("word_write/byte_read \t %04hx == %04hx\n", wres2, w1);
+    Adress adr_test_4 = 8;
+    byte b1_test_4 = 0x0b;
+    byte b0_test_4 = 0x0a;
+    word w_test_4 = 0x0b0a;
+    b_write(adr_test_4, b0_test_4);
+    b_write(adr_test_4 + 1, b1_test_4);
+    word wres_test_4 = w_read(adr_test_4);
+    printf("byte_write/word_read \t %04hx == %02hhx%02hhx\n", wres_test_4, b1_test_4, b0_test_4);
+    assert(w_test_4 == wres_test_4);
 }
 
 int main(){
