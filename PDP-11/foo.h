211c1352c74d328b0adf40265d8982ce85f9b5a3
SofiMi <maslennikova.si@phystech.edu>
2022-04-21 03:10:14 +0300

    Программа разбита на логические блоки

---
+++ PDP-11/foo.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef unsigned char byte; //8 bit
+typedef unsigned short int word; //16 bit
+typedef word Adress;        // 16 bit
+
+#define MEMSIZE (64*1024)
+
+void b_write(Adress adr, byte b);
+byte b_read(Adress adr);
+void w_write(Adress adr, word w);
+word w_read(Adress adr);
+void load_file(const char * filename);
+void test_mem();
+void test_load();
--- PDP-11/read_from_file.c
+++ PDP-11/read_from_file.c
@@ -1,21 +1,5 @@
 #include "foo.h"
 
-void b_write(Adress adr, byte b);
-byte b_read(Adress adr);
-void w_write(Adress adr, word w);
-word w_read(Adress adr);
-void load_file(const char * filename);
-void test_mem();
-void test_load();
-
-
-int main(int argc, char* argv[]){
-    test_mem();
-    load_file(argv[1]);
-    test_load();
-    return 0;
-}
-
 void load_file(const char * filename)
 {
     FILE * fin  = fopen(filename, "r");
